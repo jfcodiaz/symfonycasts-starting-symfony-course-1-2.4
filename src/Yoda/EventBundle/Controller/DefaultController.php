673c48468a51d1a356c7d06afb7399768416f19c
Francisco Díaz <jdiaz@medtrainer.com>
2020-02-05 15:54:37 -0600

    intro to params and routes

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($firstName, $count)
     {
-        return $this->render('YodaEventBundle:Default:index.html.twig');
+        return $this->render('YodaEventBundle:Default:index.html.twig', compact('firstName', 'count'));
     }
 }
